c417ace82a78423e85e600f87e870e19c5e894cc
TrungTSG <tsg.trung.2001@gmail.com>
2022-10-06 19:04:59 +0700

    .

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] private float maxHealthPlayer;
+    [SerializeField] private float currentHealthPlayer;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
